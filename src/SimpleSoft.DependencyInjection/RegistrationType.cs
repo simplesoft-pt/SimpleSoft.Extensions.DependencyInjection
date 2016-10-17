1ad212baaa33aaba4fb45454fd66bb5567d04ce1
João Simões <nogravity00@outlook.com>
2016-10-17 23:34:15 +0100

    Created the ServiceAttribute class

---
+++ src/SimpleSoft.DependencyInjection/RegistrationType.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleSoft.DependencyInjection
+{
+    /// <summary>
+    /// The registration type
+    /// </summary>
+    public enum RegistrationType
+    {
+        /// <summary>
+        /// Both interfaces and derived classes will be used
+        /// </summary>
+        All,
+        /// <summary>
+        /// Only implemented interfaces will be used
+        /// </summary>
+        InterfacesOnly,
+        /// <summary>
+        /// Only derived classes will be used
+        /// </summary>
+        DerivedClassesOnly
+    }
+}
