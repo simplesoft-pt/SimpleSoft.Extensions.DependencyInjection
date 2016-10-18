2a25c101c690ef73844edb8e29738ed57e8ab134
João Simões <nogravity00@outlook.com>
2016-10-18 16:01:48 +0100

    Added tests for service attribute scan

--- test/SimpleSoft.DependencyInjection.Tests/TestUtils.cs
+++ test/SimpleSoft.DependencyInjection.Tests/TestUtils.cs
@@ -6,12 +6,46 @@
 
     public static class TestUtils
     {
+        private static readonly IServiceProvider ServiceProvider;
+
+        static TestUtils()
+        {
+            var services = new ServiceCollection().AddServicesFrom<IMockService>();
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
         public static void UsingConfiguredServiceProvider(Action<IServiceProvider> action)
         {
             if (action == null) throw new ArgumentNullException(nameof(action));
+            
+            action(ServiceProvider);
+        }
 
-            var services = new ServiceCollection().AddServicesFrom<IMockService>();
-            action(services.BuildServiceProvider());
+        public static TResult UsingConfiguredServiceProvider<TResult>(Func<IServiceProvider, TResult> action)
+        {
+            if (action == null) throw new ArgumentNullException(nameof(action));
+            
+            return action(ServiceProvider);
+        }
+
+        public static void UsingConfiguredServiceProviderScope(Action<IServiceScope> action)
+        {
+            if (action == null) throw new ArgumentNullException(nameof(action));
+            
+            using (var scope = ServiceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                action(scope);
+            }
+        }
+
+        public static TResult UsingConfiguredServiceProviderScope<TResult>(Func<IServiceScope, TResult> action)
+        {
+            if (action == null) throw new ArgumentNullException(nameof(action));
+            
+            using (var scope = ServiceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                return action(scope);
+            }
         }
     }
 }
